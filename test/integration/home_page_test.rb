8c836abd064cea6d98cfb291131131682a81bb3e
Jason Hsu <jhsu802701@jasonhsu.com>
2019-01-30 19:29:12 +0000

    Added home page test for logged in user

--- test/integration/home_page_test.rb
+++ test/integration/home_page_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class HomePageTest < ActionDispatch::IntegrationTest
   test 'main page without login is introduction page' do
     get root_path
+    assert_select "a[href=?]", login_path
     assert_select 'h1', text: 'Octobox'
     assert_select 'h3', text: 'Untangle your GitHub Notifications'
     assert_select 'h3', text: 'Sound like you?'
@@ -17,5 +18,18 @@ class HomePageTest < ActionDispatch::IntegrationTest
     assert_match 'There are a number of install options', response.body
     assert_select 'h3', text: 'Contribute'
     assert_match 'You can also help triage issues.', response.body
+  end
+
+  test 'main page with login is notifications page' do
+    @user = create(:user)
+    sign_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", logout_path
+    assert_match 'Inbox', response.body
+    assert_match 'Archive', response.body
+    assert_match 'Starred', response.body
+    assert_match 'Read', response.body
+    assert_match 'Unread', response.body
+    assert_match 'Last sync', response.body
   end
 end
