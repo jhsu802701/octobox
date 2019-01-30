47d0012b8e2926e4e82e15eb8564e768de13d5ae
Jason Hsu <jhsu802701@jasonhsu.com>
2019-01-30 03:10:52 +0000

    Moved part of config_env.sh to build_fast.sh; execute config_env.sh only if .env file is not present

--- build_fast.sh
+++ build_fast.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-sh config_env.sh
+echo 'Welcome to the unofficial build script of the Octobox site!'
+echo 'This script assumes that you are using the Ruby on Racetracks'
+echo 'protocols to work on this project.'
+echo ''
+echo 'The purpose of this script is to allow you to quickly and efficiently'
+echo 'set up this project.  Setting up the project so that all tests pass'
+echo 'will be something you can do in minutes instead of hours.'
+echo ''
+echo 'Before you continue, please do the following:'
+echo '1. Start a second tab in LXTerminal.  Enter the command "sh join.sh".'
+echo '   After you enter the Docker container, enter the command'
+echo '   "cd octobox; redis-server".'
+echo '2. Start a third tab in LXTerminal.  Enter the command "sh join.sh".'
+echo '   After you enter the Docker container, enter the command'
+echo '   "cd octobox".  Use this tab for entering commands.'
+echo '--------------------------------------------------------------'
+echo 'When you have satisfied the above requirements, press ENTER to' 
+echo 'continue.'
+echo 'Otherwise, press Ctrl-C to exit.'
+echo '--------------------------------'
+read cont
+
+if [ ! -f .env ]; then
+  sh config_env.sh
+fi
 
 # PostgreSQL
 DB_NAME='octobox_test'
