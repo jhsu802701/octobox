3bac59bddf3a3be4d9a1240a0100dcb3c6f31122
Jason Hsu <jhsu802701@jasonhsu.com>
2019-02-17 17:31:17 +0000

    Removed DISABLE_SPRING

--- test_app.sh
+++ test_app.sh
@@ -8,8 +8,8 @@ echo '----------------'
 echo 'rails db:migrate'
 rails db:migrate
 
-echo '---------------------------------------------------------------------------------------------'
-echo 'DISABLE_SPRING=1 RAILS_ENV=test bundle exec rake --trace rubocop db:migrate test:skip_visuals'
-DISABLE_SPRING=1 RAILS_ENV=test bundle exec rake --trace rubocop db:migrate test:skip_visuals
+echo '----------------------------------------------------------------------------'
+echo 'RAILS_ENV=test bundle exec rake --trace rubocop db:migrate test:skip_visuals'
+RAILS_ENV=test bundle exec rake --trace rubocop db:migrate test:skip_visuals
 
 rm dump.rdb
